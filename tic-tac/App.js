d7caef1ced1078df627a7630f6a03160edcbadbd
Kent Davis <kentcharlesdavis@gmail.com>
2026-03-12 16:13:33 +1100

    Finished tic-tac-toe

--- tic-tac/App.js
+++ tic-tac/App.js
@@ -4,8 +4,62 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.outerBorder}>
+        <View style={styles.grid}>
+          <View style={styles.box}>
+            <Text style={styles.text}>
+              O
+            </Text>
+          </View>
+          <View style={styles.box}>
+            <Text style={styles.text}>
+              
+            </Text>
+          </View>
+          <View style={styles.box}>
+            <Text style={styles.text}>
+              O
+            </Text>
+          </View>
+
+
+          <View style={styles.box}>
+            <Text style={styles.text}>
+              X
+            </Text>
+          </View>
+          <View style={styles.box}>
+            <Text style={styles.text}>
+              X
+            </Text>
+          </View>
+          <View style={styles.box}>
+            <Text style={styles.text}>
+              O
+            </Text>
+          </View>
+
+
+
+          <View style={styles.box}>
+            <Text style={styles.text}>
+              X
+            </Text>
+          </View>
+          <View style={styles.box}>
+            <Text style={styles.text}>
+              
+            </Text>
+          </View>
+          <View style={styles.box}>
+            <Text style={styles.text}>
+              O
+            </Text>
+          </View>
+          
+        </View>  
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -13,8 +67,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  grid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    backgroundColor: 'black',
+    borderWidth: 25,
+    borderColor: 'orange',
+    borderRadius: 20,
+    width: 350,
+    height: 350,
+  },
+  text: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: 'white',
+
+  },
+  box: {
+    width: 96,
+    height: 96,
+    backgroundColor: 'green',
+
+    margin: 2,
+    borderColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  outerBorder: {
+    borderWidth: 3,
+    borderColor: 'black',
+    borderRadius: 23,
+  }
 });
